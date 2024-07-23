48955022fd68128c9db0ab0d62136591c8e26947
Chiragcs8 <chirag98589@gmail.com>
2024-07-24 00:21:02 +0530

    Starting My JS Journey

---
+++ JavaScript/Tutorial/T1.js
@@ -0,0 +1,17 @@
+alert("Hello World!")
+
+
+console.log("Hello its me Chirag")
+
+var a = prompt("Enter your number")
+var isTrue = prompt("Are you sure you want to leave this page and get stolen your all data")
+if (isTrue == "yes") {
+    console.log("stealling your data")
+}
+else{
+    console.log("Your data is not getting stolen")
+
+}
+
+
+console.log("Your number is "+a)
