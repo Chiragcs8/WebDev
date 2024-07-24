eabefbd94338331c05519dcac64370adc145c4c9
Chiragcs8 <chirag98589@gmail.com>
2024-07-24 21:40:50 +0530

    Tutorial about variables and objects

---
+++ JavaScript/Tutorial/T2.js
@@ -0,0 +1,34 @@
+console.log("Hey this is T2")
+
+let a = 5;
+let b = 6;
+let c = "Hello";
+var d = 8
+let x = 8.6;
+let y = undefined;
+let z = null;
+const p = true;
+
+
+
+console.log(a + b + 8)
+console.log(c)
+console.log(typeof a , typeof b , typeof c, typeof d, typeof x, typeof y, typeof z, typeof p)
+
+{
+    let a = 10;
+    console.log(a)
+    var d = 9
+    console.log(d)
+    
+}
+console.log(d)
+let o ={
+    name : "Xebec",
+    "job code" : "126"
+}
+console.log(o)
+o.salary = "1cr"
+console.log(o)
+o.salary = "2cr"
+console.log(o)
