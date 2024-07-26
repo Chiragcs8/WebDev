96a203e9aaf506bf8cc1d5acab7279c8a9b2db2d
Chiragcs8 <chirag98589@gmail.com>
2024-07-26 22:35:42 +0530

    Learn diffenrt features of string

---
+++ JavaScript/Tutorial/T6.js
@@ -0,0 +1,27 @@
+console.log("This is tutorial about string")
+let a = "Xebec";
+console.log(a.length);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+
+let real_name = "Chirag";
+
+let friend = "Harsh";
+
+console.log("His name is " + real_name + " His friend name is " + friend);
+
+console.log (`His name "is" ${real_name} His friend name is ${friend}`);
+
+console.log(friend.toLowerCase());
+console.log(friend.toUpperCase());
+console.log(friend.slice(1, 5));
+console.log(friend.slice(1));
+
+console.log(friend.replace("sh","77"));
+
+console.log(friend.concat(friend, "Yadav"))
+
+console.log(friend)
