45fce6544d23ae2b3c20464e96e56f8f29076df0
Chiragcs8 <chirag98589@gmail.com>
2024-07-27 20:22:01 +0530

    Learning about DOM in js

---
+++ JavaScript/Tutorial/T9.js
@@ -0,0 +1,16 @@
+console.log("This tutorial about DOM")
+
+document.body.firstElementChild
+
+document.body.firstElementChild.childNodes
+
+document.body.firstElementChild.children
+
+document.body.firstElementChild.children[3].previousElementSibling
+
+document.body.firstElementChild.children[3].previousSibling
+
+document.body.firstElementChild.children[3]
+
+document.body.firstElementChild.children[3].parentElement
+
