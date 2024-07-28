a33c3833ac2c1feca6a10170c9e294e8cf635166
Chiragcs8 <chirag98589@gmail.com>
2024-07-28 23:04:07 +0530

    learning inserting and removing elemnets through DOM

---
+++ JavaScript/Tutorial/T11.js
@@ -0,0 +1,9 @@
+let div = document.createElement("div");
+div.innerHTML = "i have been inserted by <b>Xebec</b>";
+
+// div.setAttribute("class", "created")
+// // document.querySelector(".container").append(div)
+// document.querySelector(".container").before(div)
+
+let cont = document.querySelector(".container")
+cont.insertAdjacentHTML("afterend","<b> i am under the water please help me here to much rainig....huuuuooooo")
