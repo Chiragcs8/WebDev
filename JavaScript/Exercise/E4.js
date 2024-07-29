04a5ed9b1da83c2a8dd0f07c00a11e0f47f3bd96
Chiragcs8 <chirag98589@gmail.com>
2024-07-29 19:00:52 +0530

    exercise about random color genrator

---
+++ JavaScript/Exercise/E4.js
@@ -0,0 +1,18 @@
+console.log("assinging random colors to boxes using DOM")
+
+
+function RandomColor() {
+   let r = parseInt(Math.random()*255)
+   let g = parseInt(Math.random()*255)
+   let b = parseInt(Math.random()*255)
+   return `rgb(${r}, ${g}, ${b})`
+}
+
+console.log(RandomColor())
+
+
+
+document.querySelectorAll(".box").forEach(element => {
+    element.style.backgroundColor = `${RandomColor()}`
+});
+
