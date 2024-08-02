5c1c88433015eebc5e98be55291b8da8af9b50a2
Chiragcs8 <chirag98589@gmail.com>
2024-08-02 23:56:15 +0530

    Learning class & objects and learning oop in js

---
+++ JavaScript/Tutorial/T16.js
@@ -0,0 +1,79 @@
+// let obj = {
+//     a:1,
+//     b:"Xebec"
+// }
+
+
+// let animal = {
+//     eat: true
+// }
+
+// let cheetah = {
+//     runs: true
+// }
+
+// cheetah.__proto__ = animal;
+
+// console.log(obj)
+
+class Animal{
+    constructor(name){
+        this.name = name
+        console.log("Object is created...")
+    }
+
+    eat(){
+        console.log("Eating...")
+    }
+
+    run(){
+        console.log("Running...")
+    }
+}
+
+class lion extends Animal{
+    constructor(name){
+        super(name)
+        console.log("Object is created and he is lion...")
+    }
+
+    eat(){
+        super.eat()
+        console.log("Lion is eating...")
+    }
+    
+}
+
+
+
+let a = new Animal("Cheetah"); 
+console.log(a)
+
+let l = new lion("lioness"); 
+console.log(l)
+
+
+class User {
+    constructor(name) {
+        this.name = name;
+    }
+
+    get name (){
+        return this._name;
+    }
+
+    set name (value) {
+        if (value.lengh < 4){
+            console.log("name is too short.");
+            return;
+        }
+        this._name = value;
+    }
+}
+
+let user = new User("Ram");
+console.log(user.name);
+
+user = new User ("");
+// user.name = "Xebec"
+// console.log(user.name);
