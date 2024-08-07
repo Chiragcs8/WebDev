ed3980b15e135a7fcbf3b1addbd1e61232cb9c10
Chiragcs8 <chirag98589@gmail.com>
2024-08-07 23:13:27 +0530

    learning Dom more

---
+++ JavaScript/Tutorial/T18.js
@@ -0,0 +1,44 @@
+console.log("Learning DOM in depth(deep)");
+
+// console.log(document.links(2))
+// console.log(document.baseURI)
+
+// document.getElementById('firstHeading')
+// <h1 id=​"firstHeading" class=​"firstHeading mw-first-heading">​…​</h1>​
+// document.getElementById('firstHeading').innerHTML = "<h1>Saber of Xebec</h1>"
+// '<h1>Saber of Xebec</h1>'
+
+document.getElementById("title").getAttribute
+
+document.getElementById("title").getAttribute('id')
+
+document.getElementById("title").getAttribute('class')
+
+document.getElementById("title").className
+
+document.getElementById("title").id
+
+document.getElementById("title").setAttribute('class','test')
+
+const title = document.getElementById('title')
+
+title
+
+title.style.backgroundColor = 'gray'
+
+title.style.padding = "15px"
+
+title.style.borderRadius = "15px"
+
+title.innerText
+
+title.innerHTML
+
+title.textContent
+
+console.log(document.querySelector('.heading'))
+
+console.log(document.querySelector('#title'))
+
+console.log(document.querySelector('h1'))
+
