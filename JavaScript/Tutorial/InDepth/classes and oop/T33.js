99cf9c1279da0db256c14d63dd75916a32170c2f
Chiragcs8 <chirag98589@gmail.com>
2024-08-20 23:12:00 +0530

    learning about properties based getter setter in js

---
+++ JavaScript/Tutorial/InDepth/classes and oop/T33.js
@@ -0,0 +1,34 @@
+console.log("learning about properties based getter setter in js");
+
+function User(email, password) {
+    
+    this._email = email;
+    this._password = password
+
+    Object.defineProperty(this, "email", {
+        get:function (){
+            
+            return this._email.toUpperCase()
+        },
+        set:function (value){
+
+            this._email = value;
+        }
+    })
+    
+    Object.defineProperty(this, "password", {
+        get: function () {
+            return this._password.toUpperCase()
+        },
+        set: function (value) {
+            this._password = value
+        }
+    })
+}
+
+
+
+const chai = new User("chai@bcd.com", "efg")
+
+console.log(chai.email);
+console.log(chai.password);
