ca061b0406b00cfc967c0f922b41362aae787769
Chiragcs8 <chirag98589@gmail.com>
2024-08-23 23:57:36 +0530

    learning about more through noral react

--- ReactJs/01basicreact/src/App.js
+++ ReactJs/01basicreact/src/App.js
@@ -1,7 +1,12 @@
+import Greetings from "./greetings";
+
 
 function App() {
   return (
+    <>
     <h1>Welcome to island</h1>
+    <Greetings />
+    </>
   );
 }
 
---
+++ ReactJs/01basicreact/src/greetings.js
@@ -0,0 +1,8 @@
+function Greetings() {
+    return(
+        <h2>The last Island in pradise</h2>
+    )
+}
+
+
+export default Greetings;
