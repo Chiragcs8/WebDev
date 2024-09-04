a8af3f076f5037d8478dc61d24e599429444ec10
Chiragcs8 <chirag98589@gmail.com>
2024-09-04 22:37:36 +0530

    Finishng Project about ReactRouter

--- ReactJs/Tutorials/07reactRouter/src/main.jsx
+++ ReactJs/Tutorials/07reactRouter/src/main.jsx
@@ -7,6 +7,7 @@ import About from './components/About/About.jsx'
 import Home from './components/Home/Home.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 
 // const router = createBrowserRouter([
 //   {
@@ -36,6 +37,11 @@ const router = createBrowserRouter(
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
       <Route path='user/:userid' element={<User />} />
+      <Route 
+      loader={githubInfoLoader}
+      path='github' 
+      element={<Github />}
+       />
     </Route>
   )
 )
