7b1106fc8e0e88e15d0b2b68c46c2cc6a81dc443
Chiragcs8 <chirag98589@gmail.com>
2024-09-17 21:34:54 +0530

    revicing js

--- JavaScript/Tutorial/InDepth/classes and oop/T26.js
+++ JavaScript/Tutorial/InDepth/classes and oop/T26.js
@@ -1,18 +1,16 @@
 console.log("learning about Call and This");
- 
-function SetUsername(username){
-    this.username = username
-    console.log("called");
-    
+
+function SetUsername(username) {
+  this.username = username;
+  console.log("called");
 }
 
-function createUser(username, email, password){
-    SetUsername.call(this, username)
+function createUser(username, email, password) {
+  SetUsername.call(this, username);
 
-
-    this.email = email
-    this.password = password
+  this.email = email;
+  this.password = password;
 }
 
-const chai = new createUser("chai", "chai@fb.com", "123")
+const chai = new createUser("chai", "chai@fb.com", "123");
 console.log(chai);
