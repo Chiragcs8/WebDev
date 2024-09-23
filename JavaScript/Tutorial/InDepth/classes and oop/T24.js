c01fc0caa997adf7ee451313be96d22c041e7790
Chiragcs8 <chirag98589@gmail.com>
2024-09-23 21:26:58 +0530

    revicing concept of  objects

--- JavaScript/Tutorial/InDepth/classes and oop/T24.js
+++ JavaScript/Tutorial/InDepth/classes and oop/T24.js
@@ -1,35 +1,34 @@
-console.log("Learning about Object")
+console.log("Learning about Object");
 
-function multiplyBy5(num){
-    return num*5
+function multiplyBy5(num) {
+  return num * 5;
 }
 
-multiplyBy5.power = 2
+multiplyBy5.power = 2;
 
 console.log(multiplyBy5(5));
 console.log(multiplyBy5.power);
 console.log(multiplyBy5.prototype);
 
 function createUser(username, score) {
-    this.username = username
-    this.score = score
+  this.username = username;
+  this.score = score;
 }
 
 createUser.prototype.increment = function () {
-    this.score++    
-}
-createUser.prototype.printme = function(){
-    console.log(`price is ${this.score}`);
-    
-}
+  this.score++;
+};
+createUser.prototype.printme = function () {
+  console.log(`price is ${this.score}`);
+};
 
-const chai = new createUser("chai", 25)
-const tea = new createUser("tea", 250)
+const chai = new createUser("chai", 25);
+const tea = new createUser("tea", 250);
 
-chai.printme()
-tea.printme()
-tea.increment()
-tea.printme()
+chai.printme();
+tea.printme();
+tea.increment();
+tea.printme();
 
 /*
 
