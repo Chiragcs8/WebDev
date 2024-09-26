c6d9a26b42a07dad101173998684bf5b5b43490d
Chiragcs8 <chirag98589@gmail.com>
2024-09-26 21:59:36 +0530

    revicing whole html css and js

--- JavaScript/Tutorial/FastPhase/T13.5.js
+++ JavaScript/Tutorial/FastPhase/T13.5.js
@@ -1,33 +1,30 @@
-console.log('This tutorial about Promises');
+console.log("This tutorial about Promises");
 
-console.log('This is Promises');
+console.log("This is Promises");
 
-let prom1 = new Promise((resolve, reject)=>{
-    let a = Math.random();
-    if(a < 0.5){
-        reject("O random number was not supporting you")
-    }
-    else{
+let prom1 = new Promise((resolve, reject) => {
+  let a = Math.random();
+  if (a < 0.5) {
+    reject("O random number was not supporting you");
+  } else {
     setTimeout(() => {
-        console.log('Yes i am done');
-        resolve("Xebec")
-    })
-    }
-})
+      console.log("Yes i am done");
+      resolve("Xebec");
+    });
+  }
+});
 
-let prom2 = new Promise((resolve, reject)=>{
-    let a = Math.random();
-    if(a < 0.5){
-        reject("O random number was not supporting you 2")
-    }
-    else{
+let prom2 = new Promise((resolve, reject) => {
+  let a = Math.random();
+  if (a < 0.5) {
+    reject("O random number was not supporting you 2");
+  } else {
     setTimeout(() => {
-        console.log('Yes i am done 2');
-        resolve("Xebec 2")
-    })
-    }
-})
-
+      console.log("Yes i am done 2");
+      resolve("Xebec 2");
+    });
+  }
+});
 
 // prom1.then((a)=>{
 //     console.log(a);
@@ -35,11 +32,10 @@ let prom2 = new Promise((resolve, reject)=>{
 //     console.log(err);
 // })
 
-let p3 = Promise.any([prom1, prom2])
+let p3 = Promise.any([prom1, prom2]);
 
-p3.then((a)=>{
-    console.log(a);
-}).catch(err=>{
-    console.log(err);
-})
-
+p3.then((a) => {
+  console.log(a);
+}).catch((err) => {
+  console.log(err);
+});
