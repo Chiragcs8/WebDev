b9a07aca009268f327a85442a711679e963b4403
Chiragcs8 <chirag98589@gmail.com>
2024-10-09 21:48:18 +0530

    completing appwrite config

--- ReactJs/Tutorials/MegaProjectBlog/src/appwrite/config.js
+++ ReactJs/Tutorials/MegaProjectBlog/src/appwrite/config.js
@@ -78,8 +78,54 @@ export class Service{
         }
     }
 
-    async getPosts(querries = [Query.equal]){
+    async getPosts(queries = [Query.equal]){
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries,
 
+
+            )
+        } catch (error) {
+            console.log("Appwrite server error:: getPosts :: error", error);
+        }
+    }
+
+    // file upload service
+
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite server error:: uploadFile :: error", error);
+            return false
+            
+        }
+    }
+
+    async deleteFile(fileId){
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite server error:: deleteFile :: error", error);
+            return false
+        }
+    }
+
+    getfilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
     }
 }
 
