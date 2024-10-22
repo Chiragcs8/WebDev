fb507db1dfb9b5080816a581830b33135677859a
Chiragcs8 <chirag98589@gmail.com>
2024-10-22 22:57:17 +0530

    completing Header and making button component for whenever i need it in project

--- ReactJs/Tutorials/MegaProjectBlog/src/components/Header/Header.jsx
+++ ReactJs/Tutorials/MegaProjectBlog/src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
   const authStatus = useSelector((state) => state.auth.status);
   const navigate = useNavigate();
 
-  const naItems = [
+  const navItems = [
     {
       name: "Home",
       slug: "/",
@@ -46,7 +46,20 @@ function Header() {
             </Link>
           </div>
           <ul className="flex ml-auto">
-            
+            {navItems.map((item) => 
+            item.active ? (
+              <li key={item.name}>
+                <button
+                onClick={() => navigate(item.slug)}
+                className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full">{item.name}</button>
+              </li>
+            ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
           </ul>
         </nav>
       </Container>
