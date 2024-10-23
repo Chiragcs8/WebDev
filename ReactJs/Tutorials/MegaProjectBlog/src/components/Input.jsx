525ce2b33bd09598c7b31248049ceead00b1ddff
Chiragcs8 <chirag98589@gmail.com>
2024-10-24 00:23:04 +0530

    completing Input component

--- ReactJs/Tutorials/MegaProjectBlog/src/components/Input.jsx
+++ ReactJs/Tutorials/MegaProjectBlog/src/components/Input.jsx
@@ -8,8 +8,17 @@ const Input = React.forwardRef( function Input({
 }, ref){
     return (
         <div className='w-full'>
-            {label && <label className='block mb-1' htmlFor={props.id}>{label}
+            {label && <label 
+            className='block mb-1 pl-1' htmlFor={id}>
+                {label}
                 </label>}
+                <input 
+                type={type}
+                className={ ` px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-200 w-full ${classname}`}
+                ref={ref} 
+                {...props}
+                id={id}
+                />
         </div>
     )
 })
