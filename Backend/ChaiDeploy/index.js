0b24cfc6efe53f4436ba1b874a7a5530f3ec0a03
Chiragcs8 <chirag98589@gmail.com>
2025-01-01 23:08:24 +0530

    making code look more better

--- Backend/ChaiDeploy/index.js
+++ Backend/ChaiDeploy/index.js
@@ -1,26 +1,25 @@
-require('dotenv').config()
-const express = require('express')
+require("dotenv").config();
+const express = require("express");
 
-const app = express()
+const app = express();
 
-const port = 5000
+const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/twitter', (req, res) => {
-    res.send('Xebecdotcom')
-})
+app.get("/twitter", (req, res) => {
+  res.send("Xebecdotcom");
+});
 
-app.get('/login', (req, res) => {
-    res.send('<h1> Welcome! please login at Saber of Xebec </h1>')
-})
+app.get("/login", (req, res) => {
+  res.send("<h1> Welcome! please login at Saber of Xebec </h1>");
+});
 
-app.get('/youtube', (req, res) => {
-    res.send("<h2>Xebec's YouTube</h2>")
-})
+app.get("/youtube", (req, res) => {
+  res.send("<h2>Xebec's YouTube</h2>");
+});
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+  console.log(`Example app listening on port ${port}`);
+});
