a4ac1674c015ed6c70b0216d156d5bcaea6668b9
Chiragcs8 <chirag98589@gmail.com>
2025-01-01 23:06:22 +0530

    changing little things

---
+++ Backend/ChaiDeploy/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('Xebecdotcom')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1> Welcome! please login at Saber of Xebec </h1>')
+})
+
+app.get('/youtube', (req, res) => {
+    res.send("<h2>Xebec's YouTube</h2>")
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
