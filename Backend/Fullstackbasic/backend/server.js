32429cb1cea25a8d9ea70c46f22f5921f7c0a47a
Chiragcs8 <chirag98589@gmail.com>
2025-01-05 23:42:22 +0530

    small changes in code and revision of proxy and cors

--- Backend/Fullstackbasic/backend/server.js
+++ Backend/Fullstackbasic/backend/server.js
@@ -1,8 +1,8 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 
 // app.get('/', (req, res) => {
 //   res.send('Server is Ready');
@@ -10,37 +10,36 @@ app.use(express.static('dist'))
 
 // get a list of 5 jokes
 
-app.get('/api/jokes', (req, res) => {
-   const jokes = [
+app.get("/api/jokes", (req, res) => {
+  const jokes = [
     {
       id: 1,
-      title: 'A joke',
-      content: 'Why did the chicken cross the road? To get to the other side.'
+      title: "A joke",
+      content: "Why did the chicken cross the road? To get to the other side.",
     },
     {
       id: 2,
-      title: 'Another joke',
-      content: 'Why did the cat cross the road? To get to the other side.'
+      title: "Another joke",
+      content: "Why did the cat cross the road? To get to the other side.",
     },
     {
       id: 3,
-      title: 'A third joke',
-      content: 'Why did the dog cross the road? To get to the other side.'
+      title: "A third joke",
+      content: "Why did the dog cross the road? To get to the other side.",
     },
     {
       id: 4,
-      title: 'A fourth joke',
-      content: 'Why did the mouse cross the road? To get to the other side.'
+      title: "A fourth joke",
+      content: "Why did the mouse cross the road? To get to the other side.",
     },
     {
       id: 5,
-      title: 'A fifth joke',
-      content: 'Why did the elephant cross the road? To get to the other side.'
-    }
-   ];
-   res.send(jokes);
+      title: "A fifth joke",
+      content: "Why did the elephant cross the road? To get to the other side.",
+    },
+  ];
+  res.send(jokes);
 });
-
 
 const post = process.env.PORT || 3000;
 
