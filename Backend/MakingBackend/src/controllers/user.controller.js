dd3de4c1c58443225c538c80f4512b2f6fe133a2
Chiragcs8 <chirag98589@gmail.com>
2025-01-27 01:04:38 +0530

    fixed minor changes

--- Backend/MakingBackend/src/controllers/user.controller.js
+++ Backend/MakingBackend/src/controllers/user.controller.js
@@ -164,11 +164,11 @@ const loginUser = asyncHandler(async (req, res) => {
 })
 
 const logoutUser = asyncHandler(async (req, res) => {
-    User.findByIdAndUpdate(
+    await User.findByIdAndUpdate(
         req.user._id,
         {
-            $set: {
-                refreshToken: undefined,
+            $unset: {
+                refreshToken: 1
             }
         }, 
         {  
