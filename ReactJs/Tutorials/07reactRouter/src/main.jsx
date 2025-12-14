03800eec677e98717a43a1cfbaa42de38de67670
Chiragcs8 <chirag98589@gmail.com>
2025-12-14 10:35:52 +0530

    basic adding extra route

--- ReactJs/Tutorials/07reactRouter/src/main.jsx
+++ ReactJs/Tutorials/07reactRouter/src/main.jsx
@@ -34,6 +34,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
       <Route path='' element={<Home />} />
+      <Route path='home' element={<Home />} />
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
       <Route path='user/:userid' element={<User />} />
